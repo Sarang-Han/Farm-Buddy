1b1c14736b6c8431aeb3845c178ca30f5fe11458
Sarang Han <hangpfm0518@ewhain.net>
2024-11-16 13:10:27 +0900

    initial commit

---
+++ TouchGFX/build/MINGW32_NT-6.2/generated/images/src/__generated/image_icon_theme_images_image_brightness_6_40_40_F0C14A.svg.d
@@ -0,0 +1,5 @@
+build/MINGW32_NT-6.2/generated/images/src/__generated/image_icon_theme_images_image_brightness_6_40_40_F0C14A.svg.o: \
+ generated/images/src/__generated/image_icon_theme_images_image_brightness_6_40_40_F0C14A.svg.cpp \
+ ../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp
+
+../Middlewares/ST/touchgfx/framework/include/touchgfx/hal/Config.hpp:

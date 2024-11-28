4a9ec98e8c01a7dccf69fc0f935f2d830628c05a
Sarang Han <hangpfm0518@ewhain.net>
2024-11-29 06:36:03 +0900

    온도 오르락내리락

--- TouchGFX/gui/src/mainscreen_screen/MainScreenView.cpp
+++ TouchGFX/gui/src/mainscreen_screen/MainScreenView.cpp
@@ -22,9 +22,7 @@ void MainScreenView::tearDownScreen()
 void MainScreenView::handleTickEvent()
 {
     tickCounter++;
-
-    Unicode::snprintf(Main_TextTemperatureBuffer, MAIN_TEXTTEMPERATURE_SIZE, "%d", tickCounter);
-
+    
     if (tickCounter % 60 == 0)
     {
         if (++digitalSeconds >= 60)
@@ -42,6 +40,10 @@ void MainScreenView::handleTickEvent()
 
         // Update the clock
         digitalClock.setTime24Hour(digitalHours, digitalMinutes, digitalSeconds);
+        
+        // Calculate and update the temperature
+        int currentTemperature = 23 + (tickCounter / 60 % 3); // 23, 24, 25 반복
+        Unicode::snprintf(Main_TextTemperatureBuffer, MAIN_TEXTTEMPERATURE_SIZE, "%d", currentTemperature);
+        Main_TextTemperature.invalidate();
     }
-    Main_TextTemperature.invalidate();
 }
